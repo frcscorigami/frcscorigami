f015dd5cb1e4a53ce7b378d81bdc660678c13fe0
Justin Tervay <justin@tervay.com>
2025-02-15 12:25:13 -0500

    limit years that are broke

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -41,10 +41,7 @@ const BASE_API_URL =
   process.env.NEXT_PUBLIC_API_URL ||
   "https://us-central1-frc-scorigami.cloudfunctions.net/function-get";
 
-const YEARS = [
-  2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012,
-  2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2022, 2023, 2024, 2025,
-];
+const YEARS = [2016, 2017, 2018, 2019, 2020, 2022, 2023, 2024, 2025];
 
 const DEFAULT_YEAR = 2025;
 
